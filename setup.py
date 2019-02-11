fa8af5de9ca663f4df7223477443395a2cc2bded
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2019-02-11 10:29:39 +0100

    fixed installation issue

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     version=VERSION,
     packages=find_packages(),
     install_requires=[
-        'common_helper_extraction'
+        'common_helper_files >= 0.2'
+    ],
+    dependency_links=[
+        'https://github.com/fkie-cad/common_helper_files/tarball/master#egg=common_helper_files-0.2',
     ],
     description='Extraction support functions',
     author='Fraunhofer FKIE',
