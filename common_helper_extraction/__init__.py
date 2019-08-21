a26bccee3e3332eba22d8c7bd6e0f82935e153dc
Peter Weidenbach <peter.weidenbach@fkie.fraunhofer.de>
2019-08-21 11:41:55 +0200

    functions re-added for compatiblity reasons

--- common_helper_extraction/__init__.py
+++ common_helper_extraction/__init__.py
@@ -1,7 +1,12 @@
 from .extractor_definitions import EXTRACTOR_LIST
 from .dump import dump_files
+from .lzma import extract_lzma_streams, get_decompressed_lzma_streams
+from .padding import cut_at_padding
 
 __all__ = [
     'EXTRACTOR_LIST',
-    'dump_files'
+    'dump_files',
+    'cut_at_padding',
+    'extract_lzma_streams',
+    'get_decompressed_lzma_streams'
 ]
